7f5a84643da3be06206276ab342184d579ad818f
Isaiah Greenway <isaiah.greenway2023@baltimorecitycollege.us>
2020-07-15 14:51:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Hello, welcome to your first program!")
+print("Click run to see what this program does.")
+print("Isaiah Greenway")
+print("I do not have a favorite muscian")
+print(" I would love to go to bora bora")
+print(" i like to watch television")
+print("7/14/20")
